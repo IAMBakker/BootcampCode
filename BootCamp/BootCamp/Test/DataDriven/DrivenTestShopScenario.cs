28d07ec869e27353b1809de13e64a9702072dc92
IAMBakker <ico.bakker@gmail.com>
2018-08-03 09:06:55 +0200

    Adding browserdriven test

---
+++ BootCamp/BootCamp/Test/DataDriven/DrivenTestShopScenario.cs
@@ -0,0 +1,44 @@
+﻿using BootCamp.Test.Base.Browser;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCamp.Test.DataDriven
+{
+    [TestFixture]
+    public class DrivenTestShopScenario
+    {
+
+        protected IWebDriver driver;
+        protected WebDriverWait wait;
+        protected String browser;
+
+        public DrivenTestShopScenario(String browser)
+        {
+            this.browser = browser;
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = BrowserFactoryBasic.InitBrowser(browser);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ BootCamp/BootCamp/Test/DataDriven/FillInContactFormTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BootCamp.Pages;
+using BootCamp.Pages.Base;
+using BootCamp.Test.Base;
+using NUnit.Framework;
+
+namespace BootCamp.Test.DataDriven
+{
+    [TestFixture("Chrome")]
+    [TestFixture("Firefox")]
+    [TestFixture("Chrome")]
+    public class FillInContactFormTest : DrivenTestShopScenario
+    {
+        public FillInContactFormTest(string browser) : base(browser)
+        {
+        }
+        //[Values("Chrome", "Firefox")]
+        //String browser
+        [Test]
+        public void TestMethod1()
+        {
+            ContactUsPage page = new HomePage(driver)
+                .Header.ClickContactUs()
+                .FillContactForm("Customer service", "bootcamper@feelthepain.com", "43211234", "Ipod defect while lifting, \nneed new one");
+            String pageTitle = page.GetPageTitle();
+            String alertMessage = page.GetAlertMessageText();
+
+            Assert.AreEqual("CUSTOMER SERVICE - CONTACT US", pageTitle);
+            Assert.AreEqual("Your message has been successfully sent to our team.", alertMessage);
+        }
+    }
+}
